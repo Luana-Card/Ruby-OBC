f9e739656c46bdc1d64b4744dcf10ca54c6f5fc5
Luana Cardoso <luanacoder@gmail.com>
2022-03-27 15:55:55 -0300

    Arrumando branches

--- aula-01/exercicio-01.rb
+++
@@ -1,3 +0,0 @@
-# 1 - Intalar ruby na máquina
-# 2 - Exibir no console: "Curso de Ruby do One Bit Code"
-# 3 - Leitura do artigo: Ruby - Wikipedia
--- aula-01/ruby-01.rb
+++
@@ -1,7 +0,0 @@
-puts "Hello World"
-puts 150 * 9
-
-# irb - apresentação
-
-# exercícios
-puts "Curso de Ruby do One Bit Code"
